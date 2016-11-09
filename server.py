2f877d4073d290b31c7b6f4a6ba2d3308e35706e
Conor <bannonco@tcd.ie>
2016-11-09 20:43:44 +0000

    updated server

--- server.py
+++ server.py
@@ -5,21 +5,40 @@ from threading import Thread
 
 
 
-def worker
-client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket_address=('localhost',8000)
+def workers(swimmer)
+
+	while True:
+
+		socket=swimmer.get();
+		address
+		active=True
+		while active
+			receive=client_socket.recv(1024)
 
 
-client_socket.connect(socket_address)
+			if data=="KILL_SERVICE\n"
+				socket.kill()
+				active=False
 
-receive=client_socket.recv(1024)
-client_socket.close()
+			elif data[:4]=="HELO":
+				msg=data+"IP:"[ip address]"\nPort:"[port number]"\nStudentID:13319829\n"
+	
+			else
 
-if data=="KILL_SERVICE\n"
+			
+	socket.close()
 
-else if data=="HELO text\n"
-"HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:13319829\n"
-
-else
+def server(hostname,port_number,numb_of_workers)
+	sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	sock=socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+	sock.bind((hostname,port_number))
+	sock.listen(5)	
+	pool=Queue(numb_of_workers)
+	
+	for i in range(numb_of_workers)
+		thread= Thread(target=worker,args=(pool,))
+		thread.daemon=True
+		thread.start()
 
 if __name__ == '__main__':
+	server(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
